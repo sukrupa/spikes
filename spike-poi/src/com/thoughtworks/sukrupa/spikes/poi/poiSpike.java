f35b7a6a3f7bda451e558fa55983ce323ed0a23c
linda <lgoldste@thoughtworks.com>
2011-02-01 19:33:29 +0530

    [Linda]working on getting poi test file running in IntelliJ

---
+++ spike-poi/src/com/thoughtworks/sukrupa/spikes/poi/poiSpike.java
@@ -0,0 +1,13 @@
+package com.thoughtworks.sukrupa.spikes.poi;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: ThoughtWorks
+ * Date: 2/1/11
+ * Time: 5:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class poiSpike {
+}
+
+
