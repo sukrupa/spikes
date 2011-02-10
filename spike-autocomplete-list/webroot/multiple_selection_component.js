0a832dfa9b59bb3d67bd7d10f1939ce3938b8d6f
Jim Barritt <jim@planet-ix.com>
2011-02-10 19:22:43 +0530

    [Linda, Jim] Adding and removing elements working

--- spike-autocomplete-list/webroot/multiple_selection_component.js
+++ spike-autocomplete-list/webroot/multiple_selection_component.js
@@ -10,19 +10,40 @@ multiselect.ping = function() {
 	console.log("Hello");
 }
 
-multiselect.addTag = function(inputId, valueListId) {
-	var tagName = document.getElementById(inputId).value; 
+multiselect.addTag = function(selectElement, valueListId) {
+	if (multiselect.changing === true) {
+		return;
+	}
+	var tagName = selectElement.value; 
 	var valueListElement = document.getElementById(valueListId);
 	
-	console.log("Going to add a tag called [" + tagName + "]...");
+	console.log("Going to add a tag called [" + tagName + "]...	");
 	
 	multiselect.appendTagTo(tagName, valueListElement);	
+	
+	multiselect.changing=true;
+	selectElement.value = "Add";
+	multiselect.changing=false;
+
 }
 
+multiselect.currentId=0;
+multiselect.changing=false;
+
+// http://www.webdeveloper.com/forum/showthread.php?t=193924
 multiselect.appendTagTo = function(tagName, valueListElement) {
 	var selectedItemDiv = document.createElement('div');
+	selectedItemDiv.setAttribute("id", "item-" + multiselect.currentId);
 	selectedItemDiv.setAttribute("class", "selected-item");
-	selectedItemDiv.innerHTML = "" + tagName + "<a class='remove' href='javascript:' title='Remove " + tagName + "'>x</a>";
+	selectedItemDiv.innerHTML = "" + tagName + "<a class='remove' href='javascript:' onclick='multiselect.remove(\"item-" + multiselect.currentId + "\", \""+ valueListElement.id +"\");' title='Remove " + tagName + "'>x</a>";
+	multiselect.currentId = multiselect.currentId+1;
+	valueListElement.appendChild(selectedItemDiv);		
 	
-	valueListElement.appendChild(selectedItemDiv);
+}
+
+multiselect.remove = function(linkElementId, valueListElementId) {
+	console.log("Removing: " + linkElementId);	
+	var linkElement = document.getElementById(linkElementId);	
+	var valueListElement = document.getElementById(valueListElementId);
+	valueListElement.removeChild(linkElement);	
 }
