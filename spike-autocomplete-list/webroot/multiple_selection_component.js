9593d675069c7f1306b48fe84ab2c929ff0b0f78
Jim Barritt <jim@planet-ix.com>
2011-02-10 18:02:10 +0530

    [Linda, Jim] Got elements to be added

--- spike-autocomplete-list/webroot/multiple_selection_component.js
+++ spike-autocomplete-list/webroot/multiple_selection_component.js
@@ -10,7 +10,19 @@ multiselect.ping = function() {
 	console.log("Hello");
 }
 
-multiselect.addTag = function(inputId) {
+multiselect.addTag = function(inputId, valueListId) {
 	var tagName = document.getElementById(inputId).value; 
+	var valueListElement = document.getElementById(valueListId);
+	
 	console.log("Going to add a tag called [" + tagName + "]...");
+	
+	multiselect.appendTagTo(tagName, valueListElement);	
+}
+
+multiselect.appendTagTo = function(tagName, valueListElement) {
+	var selectedItemDiv = document.createElement('div');
+	selectedItemDiv.setAttribute("class", "selected-item");
+	selectedItemDiv.innerHTML = "" + tagName + "<a class='remove' href='javascript:' title='Remove " + tagName + "'>x</a>";
+	
+	valueListElement.appendChild(selectedItemDiv);
 }
